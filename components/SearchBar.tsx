451620e93c0a3d68835a4aa116f43e0874f90e48
Rui Ying <git@ruiying.io>
2023-08-15 16:11:30 -0700

    Use form for search

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,41 +1,32 @@
 "use client";
 
 import { useState } from "react";
-import { usePathname, useSearchParams, useRouter } from "next/navigation";
+import { useSearchParams } from "next/navigation";
 
 const SearchBar: React.FC = () => {
-  const router = useRouter();
-  const pathname = usePathname();
   const searchParams = useSearchParams();
   const initialQuery = searchParams.get("q") ?? "";
 
   const [query, setQuery] = useState(initialQuery);
 
-  const handleSearch = () => {
-    router.push(`${pathname}?q=${query}`);
-  };
-
   return (
-    <div className="w-full max-w-sm flex space-x-2">
+    <form className="w-full max-w-sm flex space-x-2">
       <input
         type="text"
         autoCapitalize="none"
         autoComplete="off"
         spellCheck="false"
         enterKeyHint="search"
+        name="q"
         placeholder="课程名 / 教师名"
         className="input input-bordered w-full"
         value={query}
         onChange={(e) => setQuery(e.target.value.trim())}
       />
-      <button
-        className="btn btn-primary px-6"
-        onClick={handleSearch}
-        disabled={!query}
-      >
+      <button className="btn btn-primary px-6" type="submit" disabled={!query}>
         搜索
       </button>
-    </div>
+    </form>
   );
 };
 
