20d858bbab26c3475ad12e58c4ba9d6dc14b17d9
Rui Ying <git@ruiying.io>
2023-10-13 09:48:54 +0000

    Fix markdown formatting on heading ids

--- lib/markdown.ts
+++ lib/markdown.ts
@@ -1,21 +1,30 @@
 import { unified } from "unified";
+import { Schema, defaultSchema } from "hast-util-sanitize";
 import markdown from "remark-parse";
-import externalLinks from "remark-external-links";
 import remark2rehype from "remark-rehype";
 import raw from "rehype-raw";
 import slug from "rehype-slug";
+import externalLinks from "rehype-external-links";
 import stringify from "rehype-stringify";
 import sanitize from "rehype-sanitize";
+
+const schema: Schema = {
+  ...defaultSchema,
+  attributes: {
+    ...defaultSchema.attributes,
+    h2: ["id", ["className", "sr-only"]],
+  },
+};
 
 export const markdownToHtml = async (markdownString: string) => {
   const file = await unified()
     .use(markdown)
-    .use(externalLinks, { target: "_blank", rel: ["noopener", "noreferrer"] })
     .use(remark2rehype, { allowDangerousHtml: true })
-    .use(slug)
     .use(raw)
+    .use(slug)
+    .use(externalLinks, { target: "_blank", rel: ["noopener", "noreferrer"] })
     .use(stringify)
-    .use(sanitize)
+    .use(sanitize, schema)
     .process(markdownString);
   return file.toString();
 };
