a24789bcb457a5c985fcb5d02470dbfa3294a95f
Rui Ying <git@ruiying.io>
2024-10-30 00:02:14 -0700

    Update project

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    formats: ["image/avif", "image/webp"],
+  },
   output: "standalone",
   compress: false,
   experimental: {
